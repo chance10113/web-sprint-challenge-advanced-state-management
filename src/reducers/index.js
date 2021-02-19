7159a751c902455dd2ffa6a11250ff06669b461b
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-19 12:06:00 -0600

    Have written up reducers, but not the action file for them, nor have I tested them. They look good so far

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,54 @@
+import {
+  FETCH_SMURF_LOADING,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_FAILURE,
+  ADD_SMURF,
+  EDIT_ERROR,
+} from "../actions/index";
 
 export const initialState = {
-}
+  smurfArray: [],
+  isLoading: false,
+  error: "",
+};
 
-const reducer = ()=>{
-}
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        smurfArray: action.payload,
+      };
+
+    case FETCH_SMURF_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfArray: action.payload,
+      };
+
+    case EDIT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
-
-//Task List:
-//1. Adds the following state values into the initialState:
-//  - an array of smurfs
-//  - a boolean indicating if the app is loading
-//  - a string indicating a possible error message
-
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
