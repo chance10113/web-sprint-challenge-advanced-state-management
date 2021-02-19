b58122fa0371d882c8247da57121fdae298a6596
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-19 15:00:35 -0600

    Fixed the errors returned by dispatchs in Actions, and connected APP with getSmurf

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import AddForm from './components/AddForm';
-import SmurfList from './components/SmurfList';
-import Header from './components/Header';
+// Components
+import AddForm from "./components/AddForm";
+import SmurfList from "./components/SmurfList";
+import Header from "./components/Header";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+// Actions
+import {
+  fetchSmurfLoading,
+  fetchSmurfSuccess,
+  fetchSmurfFailure,
+  addSmurf,
+  editError,
+  getSmurf,
+} from "./actions/index";
+
 class App extends Component {
+  componentDidMount() {
+    console.log("Component Did Mount")
+    getSmurf();
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
 
         <main>
-          <SmurfList/>
-          <AddForm/>
+          <SmurfList />
+          <AddForm />
         </main>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  fetchSmurfLoading,
+  fetchSmurfSuccess,
+  fetchSmurfFailure,
+  addSmurf,
+  editError,
+  getSmurf
+)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -8,40 +8,42 @@ export const EDIT_ERROR = "EDIT_ERROR";
 
 // Thunk Action Goes Here
 export const getSmurf = () => {
-    dispatchEvent({type: FETCH_SMURF_LOADING})
-    
+  return (dispatch) => {
+    dispatch({ type: FETCH_SMURF_LOADING });
+
     axios
-        .get("http://localhost:3333/smurfs")
-        .then((res) => {
-            console.log("res", res)
-            console.log("res.data", res.data)
-            // dispatch({type:FETCH_SMURF_SUCCESS, payload:res.data})
-        })
-        .catch((error) => {
-            // dispatch({type:FETCH_SMURF_FAILURE, payload:error})
-            console.log(error)
-        })
-}
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        console.log("res", res);
+        console.log("res.data", res.data);
+        dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+      })
+      .catch((error) => {
+        dispatch({ type: FETCH_SMURF_FAILURE, payload: error });
+        console.log(error);
+      });
+  };
+};
 
 export const fetchSmurfLoading = () => {
-    return ({ type: FETCH_SMURF_LOADING })
-}
+  return { type: FETCH_SMURF_LOADING };
+};
 
 export const fetchSmurfSuccess = (smurfArray) => {
-    return ({ type: FETCH_SMURF_SUCCESS, payload: smurfArray})
-}
+  return { type: FETCH_SMURF_SUCCESS, payload: smurfArray };
+};
 
 export const fetchSmurfFailure = (error) => {
-    return ({ type: FETCH_SMURF_FAILURE, payload: error})
-}
+  return { type: FETCH_SMURF_FAILURE, payload: error };
+};
 
 export const addSmurf = (smurf) => {
-    return ({type: ADD_SMURF, payload: smurf })
-}
+  return { type: ADD_SMURF, payload: smurf };
+};
 
 export const editError = (error) => {
-    return ({ type: EDIT_ERROR, payload: error })
-}
+  return { type: EDIT_ERROR, payload: error };
+};
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
